a3c8327019d68c2ab419355c8f9665a52fee5978
Marko1808 <malimrav119>
2018-12-04 00:31:05 +0100

    Placanje karticom

--- src/main/java/sep/controller/PaymentController.java
+++ src/main/java/sep/controller/PaymentController.java
@@ -38,6 +38,82 @@ public class PaymentController {
 		PaymentUrlAndId urlAndId = urlAndIdService.findById(new Long(id));
 		System.out.println("Treba da ga prebacim na url: " + urlAndId.getPaymentUrl());
 	}
+	
+	@CrossOrigin
+	@RequestMapping(
+			value = "/findCard",
+			method = RequestMethod.POST
+	)
+	public CardDTO findCard(@RequestBody CardDTO card) {
+		Long paymentId = new Long(card.getPaymentId());
+		List<Request> requestList = requestService.getAll();
+		Request trazeni = new Request();
+		
+		for(Request req : requestList) {
+			if(req.getPaymentId().compareTo(paymentId)==0) {
+				trazeni = req;
+			}
+		}
+		List<Card> listaKartica = cardService.getAll();
+		Card karticaMarchanta = new Card();
+		Long idMerchanta = new Long(trazeni.getMerchantId());
+		for(Card c : listaKartica) {
+			if(c.getMerchantId()!=null && c.getMerchantId().compareTo(idMerchanta)==0) {
+				karticaMarchanta = c;
+			}
+		}
+		CardDTO cardToReturn = new CardDTO();
+		cardToReturn.setPan(karticaMarchanta.getPan().toString());
+		return cardToReturn;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(
+			value = "/autentifikacijaUsera",
+			method = RequestMethod.POST
+	)
+	public Boolean autentifikacijaUsera(@RequestBody CardDTO card) {
+		List<Card> listaKartica = cardService.getAll();
+		Long pan = new Long(card.getPan());
+		Long securityCode = new Long(card.getSecurityCode());
+		Boolean postojiKartica = false;
+		Card karticaPlacenika = new Card();
+		Long paymentId = new Long(card.getPaymentId());
+		List<Request> requestList = requestService.getAll();
+		Request trazeni = new Request();
+		
+		for(Request req : requestList) {
+			if(req.getPaymentId().compareTo(paymentId)==0) {
+				trazeni = req;
+			}
+		}
+		
+		for(Card c : listaKartica) {
+			if(card.getCardHolderName().equals(c.getCardHolderName()) && 
+					card.getExpirationDate().equals(c.getExpirationDate()) &&
+							pan.compareTo(c.getPan())==0 && securityCode.compareTo(c.getSecurityCode())==0
+							) {
+				System.out.println("Postoji covek sa ovom platnom karticom");
+				postojiKartica = true;
+				karticaPlacenika = c;
+			}
+		}
+		if(postojiKartica) {
+			
+			if(trazeni.getId()!=null) {
+				if(karticaPlacenika.getAmount().compareTo(trazeni.getAmount())>0) {
+					return true;
+				}else {
+					return false;
+				}
+			}else {
+				return false;
+			}
+			
+		}else {
+			return false;
+		}
+	}
 
 	@CrossOrigin
 	@RequestMapping(
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,4 +10,7 @@ insert into Card (pan, SECURITY_CODE, CARD_HOLDER_NAME, EXPIRATION_DATE, MERCHAN
 insert into Card (pan, SECURITY_CODE, CARD_HOLDER_NAME, EXPIRATION_DATE, MERCHANT_ID, Amount) values 
 	(12345687, 123, 'Tamara', '2019-11', null, 550.00);	
 	
+insert into Card (pan, SECURITY_CODE, CARD_HOLDER_NAME, EXPIRATION_DATE, MERCHANT_ID, Amount) values 
+	(12345671, 123, 'Tamara', '2019-11', null, 550.00);	
+	
 	
